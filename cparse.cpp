60b3bf8c3b6190dcd227ffac3d80ea6064261ce1
codyccarmona <76072605+codyccarmona@users.noreply.github.com>
2022-03-22 11:53:21 -0400

    read production rules from stdin
    struct production rule holds all production rules. read line from stdin and pass it to production rule constructor

--- cparse.cpp
+++ cparse.cpp
@@ -1,7 +1,38 @@
 #include <iostream>
 #include <list>
 
-int main(){
+struct ProductionRule{
+    char symbol;
+    std::list<char> production;
+    
+    ProductionRule(std::string rule){
+        auto arrowEndingIndex = rule.find('>');
+        auto productionItr = rule.begin();
 
+        if(arrowEndingIndex != std::string::npos){
+            symbol = *productionItr;
+            productionItr += arrowEndingIndex + 1;               
+        }
+        else{
+            symbol = '`';           
+        }
+
+        for( ; productionItr != rule.end(); productionItr++)
+            production.push_back(*productionItr);
+        
+    }
+};
+
+int main(int argc, char *argv[]){
+    std::list<ProductionRule> grammar;
+    std::string readLine;
+
+    /** Read grammar production rules for stdin */
+    while(std::getline(std::cin, readLine)){
+        ProductionRule rule(readLine);
+        auto i = readLine.find('>');
+        grammar.push_back(rule);
+    }
+    
     return 0;
 }
