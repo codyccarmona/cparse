535e020dbfc8c60e4d2020660b83b257af36738f
codyccarmona <76072605+codyccarmona@users.noreply.github.com>
2022-03-23 18:58:57 -0400

    final working product

--- cparse.cpp
+++ cparse.cpp
@@ -1,41 +1,61 @@
+/**
+ *  Assignment: MP3 - C parse
+ *  Subject: read in a start symbol of a set of productions followed by the set of productions
+ *       from  stdin  and  produce  the  collection  of  sets  of  LR(0)  items  to  stdout
+ *  Author: Cody Carmona
+ *  FSUid: ccc13d
+ *  Compile command: g++ -std=c++11 -Wall -Wextra -o carmona.exe carmona.cpp
+ */
+
 #include <iostream>
 #include <vector>
 #include <list>
 #include <algorithm>
 
-bool isTerminal(char c){
-    return (!isupper(c) && c != '@' && c != '\'' && c != ' ');
-}
 
 struct ProductionRule{
-    ProductionRule(std::string rule){
-        auto rhsStartIndex = rule.find_first_of('>');
+    ProductionRule(){ }
 
-        if(rhsStartIndex != std::string::npos){
-            this->lhs = rule[0];
-            this->rhs = rule.substr(rhsStartIndex + 1, rule.size());
+    ProductionRule(std::string rule, char p){
+        int productionStartIndex = rule.find_first_of('>');
+        std::string formattedRule = formatRule(rule);
+        if(productionStartIndex != std::string::npos){
+            lhs = rule[0];
+            productionStartIndex++;
         }
         else{
-            this->lhs = '`';
-            this->rhs = rule;
+            lhs = '`';
+            productionStartIndex = 0;
         }
+
+        rhs = p;
+        rhs += rule.substr(productionStartIndex, rule.size());
+        parserSymbol = p;
     }
 
-    ProductionRule(const ProductionRule& p, char parserSymbol = '\0'){
-        this->lhs = p.lhs;
-        if(parserSymbol != '\0')
-            this->rhs += parserSymbol;
-        this->rhs += p.rhs;
+    ProductionRule(char l, std::string r, char p){
+        lhs = l;
+        rhs = r;
+        parserSymbol = p;
     }
 
-    ProductionRule(const ProductionRule& p, size_t parseSymbolIndex){
+    ProductionRule(const ProductionRule& p){
         this->lhs = p.lhs;
         this->rhs = p.rhs;
-        this->rhs[parseSymbolIndex] = p.rhs[parseSymbolIndex + 1];
-        this->rhs[parseSymbolIndex + 1] = p.rhs[parseSymbolIndex];
+        this->parserSymbol = p.parserSymbol;
     }
 
-    bool isLhs(char c){ 
+    ProductionRule advanceParserSymbol(){
+        ProductionRule p = ProductionRule(lhs, rhs, parserSymbol);
+        auto parserSymbolIndex = p.indexOfParseSymbol();
+
+        p.rhs[parserSymbolIndex] = p.charAfterParseSymbol();;
+        p.rhs[parserSymbolIndex + 1] = p.parserSymbol;  
+
+        return p;
+    }
+
+    bool matchesLhs(char c){ 
         return lhs == c; 
     }
 
@@ -43,126 +63,150 @@ struct ProductionRule{
         if(l.lhs != r.lhs || l.rhs.size() != r.rhs.size())
             return false;
 
-        for(auto i = 0; i < l.rhs.size(); i++){
-            if(l.rhs[i] != r.rhs[i])
-                return false;
-        }
-
-        return true;
+        return l.rhs == r.rhs;
     }
 
-    std::size_t indexOfParseSymbol(char c){
-        auto index = rhs.find(c);
-        return index;
+    friend bool operator!=(const ProductionRule& l, const ProductionRule& r){
+        return !(l == r);
     }
 
-    char symbolAtIndexAfterParse(char c){
-        auto index = indexOfParseSymbol(c);
+    char charAfterParseSymbol(){
+        auto index = indexOfParseSymbol();
         return rhs[index + 1];
-    }
-
-    char valueAt(std::size_t i){ 
-        return rhs[i]; 
     }
 
     std::string productionString(){
         std::string production = "";
         production += lhs;
-        production += "->";
+        production += + "->";
         production += rhs;
+
         return production;
     }
+
+    bool isNonterminal(char c){
+        return (std::isalpha(c) && std::isupper(c));
+    }
+
+    bool isTerminal(char c){
+        return (!isNonterminal(c) && c != '@' && c != '\'' && c != '\0');
+    }
+
 private:
+    std::string formatRule(std::string rule){
+        if(rule.find(' ') == std::string::npos)
+            return rule;
+        
+        std::string formattedRule = "";
+        for(auto ruleItr = rule.begin(); ruleItr != rule.end(); ruleItr++){
+            if(std::isspace(*ruleItr))
+                continue;
+            formattedRule += *ruleItr;
+        }
+        return formattedRule;
+    }
+
+    int indexOfParseSymbol(){
+        auto index = rhs.find(parserSymbol);
+        return index;
+    }
     char lhs;
     std::string rhs;
+    char parserSymbol;
 };
 
-struct Grammar{
-    Grammar(std::vector<std::string> stringProductions){
+struct AugmentedGrammar{
+    AugmentedGrammar(std::vector<std::string> stringProductions){
         for(auto i : stringProductions){
-            ProductionRule p = ProductionRule(i);
-            rules.push_back(p);
+            ProductionRule rule = ProductionRule(i, '@');
+            rules.push_back(rule);
         }
     }
-    std::vector<ProductionRule> matchLhs(char c){
+
+    std::vector<ProductionRule> getRulesFor(char c){
         std::vector<ProductionRule> p = std::vector<ProductionRule>();
         for(auto r : rules){
-            if(r.isLhs(c))
+            if(r.matchesLhs(c))
                 p.push_back(ProductionRule(r));
         }
         return p;
     }
-    ProductionRule ruleAt(int i){
+
+    ProductionRule ruleAtIndex(int i){
         ProductionRule p = ProductionRule(rules[i]);
         return p;
     }
-    int ruleCount(){ return rules.size(); } 
+
+    int ruleCount(){ 
+        return rules.size(); 
+    } 
+
+
+    void print(){
+        std::printf("Augmented Grammar\n");
+        std::printf("-----------------\n");
+        for(auto r : rules){
+            std::printf("%s\n", r.productionString().c_str());
+        }
+        std::printf("\n");
+    }
 private:
     std::vector<ProductionRule> rules;
 };
 
 struct Goto{
-    Goto() : parentItemIndex(0), parentItemSymbol(' '), gotoItemIndex(0) {}
-
-    Goto(size_t p, char c, size_t g = 0) : parentItemIndex(p), parentItemSymbol(c), gotoItemIndex(g) {}
-
-    Goto(const Goto& g){
-        this->parentItemIndex = g.parentItemIndex;
-        this->parentItemSymbol = g.parentItemSymbol;
-        this->gotoItemIndex = g.gotoItemIndex;
+    Goto(char s, int i){
+        symbol = s;
+        sourceRuleIndex = i;
     }
 
-    bool isSet(){ 
-        return parentItemSymbol != ' '; 
+    void setdestinationRuleIndex(int i){
+        destinationRuleIndex = i;
     }
 
-    bool isSymbol(char c){ 
-        return parentItemSymbol == c; 
-    }
-    
-    size_t gotoIndex(){ 
-        return gotoItemIndex; 
+    int getSourceRuleIndex(){
+        return sourceRuleIndex;
     }
 
-    char symbol(){
-        return this->parentItemSymbol;
+    int getDestinationRuleIndex(){
+        return destinationRuleIndex;
+    }
+
+    friend bool operator==(const Goto& l, const Goto& r){
+        return l.symbol == r.symbol;
+    }
+
+    char getSymbol(){
+        return symbol;
     }
 private:
-    size_t parentItemIndex;
-    char parentItemSymbol;
-    size_t gotoItemIndex;
+    char symbol;
+    int sourceRuleIndex;
+    int destinationRuleIndex;
 };
 
 struct Item{
-    Item(Grammar g){
-        addRule(g.ruleAt(0));
-        closure(g);
-        goTos = std::vector<Goto>(rules.size());
+    Item(AugmentedGrammar grammar, ProductionRule initialRule){
+        addRule(initialRule);
+        closure(grammar, 0);
     }
 
-    Item(std::vector<ProductionRule> rules){
-        for(auto r : rules){
-            auto parserSymbolIndex = r.indexOfParseSymbol('@');
-            auto nextSymbol = r.valueAt(parserSymbolIndex + 1);
+    Item(Item item, int gotoIndex, AugmentedGrammar grammar){
+        std::vector<ProductionRule> matchedRules;
 
-            ProductionRule newRule = ProductionRule(r, parserSymbolIndex);
-            this->rules.push_back(newRule);
+        for(auto r : item.rules){
+            if(r.charAfterParseSymbol() == item.gotos[gotoIndex].getSymbol())
+                addRule(r.advanceParserSymbol());
         }
-        this->goTos = std::vector<Goto>(this->rules.size());
+        closure(grammar, 0);
     }
 
-    std::vector<ProductionRule> matchGoto(char c){
-        std::vector<ProductionRule> p = std::vector<ProductionRule>();
-        for(auto r : rules){
-            auto parserSymbolIndex = r.indexOfParseSymbol('@');
-            if(parserSymbolIndex != std::string::npos && r.valueAt(parserSymbolIndex + 1) == c)
-                p.push_back(ProductionRule(r));
-        }
-        return p;
-    }
-
-    size_t ruleCount(){ 
+    int ruleCount(){ 
         return rules.size(); 
+    }
+
+    int gotoCount(){
+        return gotos.size();
     }
 
     friend bool operator==(const Item& l, const Item& r){
@@ -170,120 +214,124 @@ struct Item{
             return false;
         
         for(auto i = 0; i < l.rules.size(); i++){
-            if(l.rules[i] == r.rules[i])
-                return true;
+            if(l.rules[i] != r.rules[i])
+                return false;
         }
 
-        return false;
+        return true;
     }
 
-    void setGoto(Goto g, int index){
-        if(goTos[index].symbol() != g.symbol())
-            goTos[index] = Goto(g);
+    void setDestinationForGoto(int gotoIndex, int destinationRuleIndex){
+        gotos[gotoIndex].setdestinationRuleIndex(destinationRuleIndex);
     }
 
-    bool hasGotoSymbol(char c){
-        for(auto g : goTos){
-            if(g.symbol() == c)
-                return true;
-        }
-        return false;
-    }
-
-    ProductionRule ruleAt(int i){
-        ProductionRule p = ProductionRule(rules[i]);
-        return p;
-    }
-
-    Goto gotoAt(int i){
-        Goto g = Goto(goTos[i]);
-        return g;
+    friend bool operator!=(const Item& l, const Item& r){
+        return !(l == r);
     }
 
     void print(){
         for(auto i = 0; i < rules.size(); i++){
-            if(goTos[i].isSet())
-                std::printf("  %-20s goto(%c)=I%d\n", rules[i].productionString().c_str(), goTos[i].symbol(), goTos[i].gotoIndex());
+            int matchedGotoIndex = findGotoWithSourceRuleIndex(i);
+
+            if(matchedGotoIndex >= 0)
+                std::printf("  %-20s goto(%c)=I%d\n", rules[i].productionString().c_str(), gotos[matchedGotoIndex].getSymbol(), gotos[matchedGotoIndex].getDestinationRuleIndex());
             
             else
                 std::printf("  %-20s\n", rules[i].productionString().c_str());
-            
         }
     }
 private:
-    void addRule(ProductionRule p){
-        if(p.indexOfParseSymbol('@') == std::string::npos)
-            p = ProductionRule(p, '@');
+    int findGotoWithSourceRuleIndex(int i){
+        for(auto j = 0; j < gotos.size(); j++){
+            if(gotos[j].getSourceRuleIndex() == i)
+                return j;
+        }
 
+        return -1;
+    }
+
+    void addRule(ProductionRule p){
         for(auto r : rules){
-            if(r == p){
+            if(r == p) 
                 return;
-            }
         }
         rules.push_back(p);
     }
 
-    void closure(Grammar g){
-        auto index = 0;
+    void closure(AugmentedGrammar grammar, int currentRuleIndex){
+        if(currentRuleIndex >= rules.size())
+            return;
 
-        while(index < rules.size()){
-            auto currentRule = rules[index];
-            auto parseSymbolIndex = currentRule.indexOfParseSymbol('@');
+        char charAfterParserSymbol = rules[currentRuleIndex].charAfterParseSymbol();
+        bool isNonTerminal = rules[currentRuleIndex].isNonterminal(charAfterParserSymbol);
 
-            if(parseSymbolIndex != std::string::npos){
-                char nextIndexChar = currentRule.valueAt(parseSymbolIndex + 1);
-                if(!isTerminal(nextIndexChar)){
-                    std::vector<ProductionRule> matchedRules = g.matchLhs(nextIndexChar);
-                    for(auto i : matchedRules){
-                        ProductionRule p = ProductionRule(i, '@');
-                        addRule(p);
-                    }
-                }
-            }
-
-            index++;
+        if(charAfterParserSymbol != '\0')
+            addGoto(Goto(charAfterParserSymbol, currentRuleIndex));
+            
+        if(isNonTerminal){
+            std::vector<ProductionRule> matchedRules = grammar.getRulesFor(charAfterParserSymbol);
+            for(auto mr : matchedRules)
+                addRule(mr);
         }
+        closure(grammar, currentRuleIndex + 1);
     }
+
+    void addGoto(Goto goTo){
+        for(auto g : gotos){
+            if(g == goTo)
+                return;
+        }
+        gotos.push_back(goTo);
+    }
+
+    std::vector<Goto> gotos;
     std::vector<ProductionRule> rules;
-    std::vector<Goto> goTos;
 };
 
 struct Set{
-    Set(Item i){
-        items.push_back(i);
-        goTos();
+    Set(AugmentedGrammar grammar){
+        items.push_back(Item(grammar, grammar.ruleAtIndex(0)));
+        go_to(grammar, 0);
     }
+
     void print(){
-        for(auto i = 0; i < items.size(); i++){
+        std::printf("Sets of LR(0) Items\n");
+        std::printf("-------------------\n");
+        for(int i = 0; i < items.size(); i++){
             std::printf("I%d:\n", i);
             items[i].print();
+            std::printf("\n");
         }
     }
 private:
-    void goTos(){
-        auto itemIndex = 0;
+    void go_to(AugmentedGrammar grammar, int currentItemIndex){
+        if(currentItemIndex >= items.size())
+            return;
 
-        while(itemIndex < items.size()){
-            Item currentItem = items[itemIndex];
-            for(auto ruleIndex = 0; ruleIndex < currentItem.ruleCount(); ruleIndex++){
-                ProductionRule currentRule = currentItem.ruleAt(ruleIndex);
-                Goto currentGoto = currentItem.gotoAt(ruleIndex);
-                size_t parseSymbolIndex = currentRule.indexOfParseSymbol('@');
-                char nextIndexSymbol = currentRule.valueAt(parseSymbolIndex + 1);
+        for(int gotoIndex = 0; gotoIndex < items[currentItemIndex].gotoCount(); gotoIndex++){      
+            Item item = Item(items[currentItemIndex], gotoIndex, grammar);
+            int foundItemIndex = findItem(item);
 
-                if(!isTerminal(nextIndexSymbol)){                 
-                    std::vector<ProductionRule> matchedGotoRules = items[itemIndex].matchGoto(nextIndexSymbol);
-                    items.push_back(Item(matchedGotoRules));
-                    if(!currentItem.hasGotoSymbol(nextIndexSymbol))
-                        items[itemIndex].setGoto(Goto(ruleIndex, nextIndexSymbol, items.size()), ruleIndex);
-                }
+            if(foundItemIndex < 0){
+                items.push_back(item);
+                items[currentItemIndex].setDestinationForGoto(gotoIndex ,items.size() - 1);
             }
-            itemIndex++;
+            else
+                items[currentItemIndex].setDestinationForGoto(gotoIndex, foundItemIndex);              
+        }      
+
+        go_to(grammar, currentItemIndex + 1);  
+    }
+    
+    int findItem(Item item){
+        for(int i = 0; i < items.size(); i++){
+            if(items[i] == item)
+                return i;
         }
+        return -1;
     }
     std::vector<Item> items;
 };
-
 
 std::vector<std::string> readGrammar(){
     std::vector<std::string> lines;
@@ -296,12 +344,11 @@ std::vector<std::string> readGrammar(){
     return lines;
 }
 
-
 int main(int argc, char *argv[]){
-    std::vector<std::string> lines = readGrammar();
-    Grammar grammar = Grammar(lines);
-    Item item = Item(grammar);
-    Set set = Set(item);
+    std::vector<std::string> grammarLines = readGrammar();
+    AugmentedGrammar augmentedGrammar = AugmentedGrammar(grammarLines);
+    Set set = Set(augmentedGrammar);
+    augmentedGrammar.print();
     set.print();
     return 0;
 }
